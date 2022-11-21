87ae8ccd0bff52f10af4a7f8b0cd0b6af5ae7ae2
JuliaShurmei <ivanovaulia428@gmail.com>
2022-11-21 12:00:00 +0300

    Fixed functionality

--- script.js
+++ script.js
@@ -35,29 +35,4 @@ function generatePassword(){
     allChars  += symbols.checked ? allSymbols : "";
 
 
-    if(allChars == "" || allChars.length == 0){
-        return genPassword;
-    }
     
-
-    let i = 1;
-    while(i<=inputSlider.value){
-        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
-        i++;
-    }
-
-    return genPassword;
-}
-
-copyIcon.addEventListener('click', ()=>{
-    if(passBox.value != "" || passBox.value.length >=1){
-        navigator.clipboard.writeText(passBox.value);
-        copyIcon.innerText = "check";
-        copyIcon.title = "Password Copied";
-
-        setTimeout(()=>{
-            copyIcon.innerHTML = "content_copy";
-            copyIcon.title = "";
-        }, 3000)
-    }
-});
